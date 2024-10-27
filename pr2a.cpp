ed9dd319c47c2ab6b98a57f0e5e470b4e9351a52
Miguerubsk <miguerubsk@gmail.com>
2024-10-27 15:23:12 +0100

    Create pr2a.cpp

---
+++ pr2a.cpp
@@ -0,0 +1,24 @@
+#include <cstdlib>
+
+#include "igvInterfaz.h"
+
+// Objetos globales
+// objeto que contiene la configuración y la interacción con la ventana de visualización
+igvInterfaz interfaz;
+
+int main (int argc, char** argv) {
+	// inicializa la ventana de visualizacion
+	interfaz.configura_entorno(argc,argv,
+                                500,500, // tama–o de la ventana
+                                100,100, // posicion de la ventana
+                                "Informatica Grafica y Visualizacion. Practica 2A." // titulo de la ventana
+                                );
+
+	// establece las funciones callbacks para la gestion de los eventos
+	interfaz.inicializa_callbacks();
+
+	// inicia el bucle de visualizacion de OpenGL
+	interfaz.inicia_bucle_visualizacion();
+
+	return(0);
+}
